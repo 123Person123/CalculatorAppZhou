23ee70bee9b370fa726e54063656410b56ae8524
Jason Z <zhoujason32@gmail.com>
2023-09-08 08:18:49 -0500

    Started function in MainActivity to get the value of the button pressed.

--- app/src/main/java/com/example/calculatorappzhou/MainActivity.java
+++ app/src/main/java/com/example/calculatorappzhou/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorappzhou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    String numInput = "";
+    String displayedInput = "0";
+
+    public void numSelected(View v){
+        if(v.getId() == R.id.b0){
+            if(!displayedInput.equals("0")){
+                numInput += "0";
+                displayedInput += "0";
+            }
+        }
+        if(v.getId() == R.id.b1){
+            if(displayedInput.equals("0")){
+                numInput = "1";
+                displayedInput = "1";
+            }
+            else{
+                numInput += "1";
+                displayedInput += "1";
+            }
+        }
+    }
+
+
 }
